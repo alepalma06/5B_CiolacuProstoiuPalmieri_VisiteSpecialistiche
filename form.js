47a9c48ea89df2b5eda9cccb5d8210517fb3de8d
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2024-11-02 14:41:24 +0100

    aggiunta commenti form

--- form.js
+++ form.js
@@ -6,13 +6,14 @@ const createForm = (parentElement) => {
         setLabels: (labels) => { data = labels; }, 
         onsubmit: (callbackInput) => { callback = callbackInput; },
         render: () => {
+            //creazione input
             parentElement.innerHTML = 
                 `<div>Data<br/><input id="data" type="date" class="form-label form-control"/></div>` +
                 `<div>Ora<br/><input id="ora" type="dropdown" class="form-label form-control"/></div>` +
                 `<div>Nome<br/><input id="nome" type="text" class="form-label form-control"/></div>` +
                 `<button type='button' id='submit' class="btn btn-primary">Conferma</button>`;
                 `<button type='button' id='annulla' class="btn btn-primary">Annulla</button>`;
-
+            //lettura valori inseriti
             document.querySelector("#submit").onclick = () => {
                 const result = {
                     data: document.querySelector("#data").value,
@@ -24,13 +25,13 @@ const createForm = (parentElement) => {
         }
     };
 };
-
+//creazione
 const formElement = document.getElementById("form");
 const form = createForm(formElement);
 
 form.onsubmit((resultform) => { // 
     console.log(resultform, table1.data);
-    table1.remove(resultform);
+    //table1.remove(resultform);
     document.querySelector("#data").value = "";
     document.querySelector("#ora").value = "";
     document.querySelector("#nome").value = "";
