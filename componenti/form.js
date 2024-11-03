59a98341169aaa03a44a8773316bfd65b65b4576
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2024-11-03 13:13:55 +0100

    form modificata con creazione chiave-valore

--- componenti/form.js
+++ componenti/form.js
@@ -1,9 +1,9 @@
 const createForm = (parentElement) => {
-    let data = {};
+    let dato = {};
     let callback = null;
     let tipo="Cardiologia";
     return {
-        setLabels: (labels) => { data = labels; }, 
+        setLabels: (labels) => { dato = labels; }, 
         onsubmit: (callbackInput) => { callback = callbackInput; },
         setType: (tip)=>{tipo=tip;console.log(tipo)},
         render: () => {
@@ -11,16 +11,31 @@ const createForm = (parentElement) => {
             parentElement.innerHTML = 
                 `<div>Data<br/><input id="data" type="date" class="form-label form-control"/></div>` +
                 `<div>Ora<br/><select id="ora" name="ora" class="form-select"><option value=8>8</option><option value=9>9</option><option value=10>10</option><option value=11>11</option><option value=12>12</option></select></div>` +
-                `<div>Nome<br/><input id="nome" type="text" class="form-label form-control"/></div>`
+                `<div>Nome<br/><input id="nome" type="text" class="form-label form-control"/></div>`+
+                `<div id="outputform"></div>`
             //lettura valori inseriti
             document.querySelector("#Prenota").onclick = () => {
-                const result = {
-                    data: document.querySelector("#data").value,
-                    ora: document.querySelector("#ora").value,
-                    nome: document.querySelector("#nome").value,
-                };
-                callback(result);  
+                const data = document.querySelector("#data").value;
+                const ora = document.querySelector("#ora").value;
+                const nome = document.querySelector("#nome").value;
+                const outputform = document.getElementById("outputform");
+
+                if (data === "" || ora === "" || nome === "") {
+                    outputform.innerHTML = "KO";
+                } else {
+                    const datasenzatrattini = data.split("-").join("");
+                    let chiave = tipo+"-"+datasenzatrattini+"-"+ora;
+                    dato[chiave] = nome;
+                    outputform.innerHTML = "OK";
+                    console.log(dato);
+                }
+
+                const result = { data, ora, nome };
+                if (callback) { 
+                    callback(result);  
+                }
             };
+
         }
     };
 };
